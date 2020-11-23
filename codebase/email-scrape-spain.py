8ed6ee6c6027adc1c561e04ffb71113d06247dbf
alibk95 <abkarimi1995@gmail.com>
2020-11-23 16:41:18 +0100

    comments added to the code

--- codebase/email-scrape-spain.py
+++ codebase/email-scrape-spain.py
@@ -1,13 +1,12 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 import time
 import pandas as pd
-import pygsheets
-from google.oauth2 import service_account
-import json
-from oauth2client.service_account import ServiceAccountCredentials
+# import pygsheets
+# from google.oauth2 import service_account
+
+# from oauth2client.service_account import ServiceAccountCredentials
 ##########################################
 
 #with open('ace-server-284012-ad303bc8a912.json') as source:
@@ -18,11 +17,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 
 
-
+# because the part that we want to scrape is actually loading with a script I used chrome driver to
+# emulate such a behavior and scroll till the end of the page and retrieve all the information needed
 url = "https://onemarketingplace.es/busqueda/diseno%20web"
+# The location of the chrome driver.
 driver = webdriver.Chrome('/usr/bin/chromedriver')
 driver.get(url)
 
+# Automatically scroll till the end of the page
 lenOfPage = driver.execute_script("window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
 match=False
 while(match==False):
@@ -32,49 +34,51 @@ while(match==False):
         if lastCount==lenOfPage:
             match=True
 
+# Give it a 10 second rest to load whatever left behind : )) and then close the browser.
 time.sleep(10)
 page = driver.page_source
 driver.quit()
 
+# Make the soup here...magic happens here!
 soup = BeautifulSoup(page, 'html.parser')
-
+# the part that is loaded by the script is accessible
 pg = soup.find("div", attrs={"class":"listado__izquierda"})
 p = pg.find_all("article", attrs={"class":"item_from_search margen_top_20"})
+
+# the base url for the more information part
 base = "https://onemarketingplace.es"
-print(len(p))
+# Just wanted to check how many records we got
+# print(len(p))
 
 # dataframe
 df = pd.DataFrame(columns=['title', 'email'])
 
 for s in p:
+    # p consists of all the different articles in the webpage and the following path leads to the "title"
     print(s.h2.text)
-
+    # retrieving the more info link to access the email information from there
     s1 = s.find("div", attrs={"class":"item_more_info"})
     link = s1.a.get("href")
+    # concat the base link and the retrieved one and ready to access that as well
     complete_link = base+link
-    #print(complete_link)
+    # print(complete_link)
     req = Request(complete_link)
     pge = urlopen(req)
+    # sp now consists of the page with information for each article. e.g. email
     sp = BeautifulSoup(pge, "html.parser")
     email = sp.find("a", attrs={"class":"contenido__correo"}).get("href")
     if email == "mailto:":
         email = ""
     print(email)
-
+    # add the info's to the data frame.
     df = df.append({'title': s.h2.text, 'email': email[7:]}, ignore_index=True)
+    # the most important thing is the object destructive function as we create a new object in each iteration
+    # there's gonna be a heavy load on the system if we keep them all
+    # So after each iteration we delete it
     del(sp)
     print("#####################")
 
 print(df)
+# Write the dataframe to a .csv file.
 df.to_csv(r'./information.csv', index = False, header=True)
-####################################################################
-#base_url = "https://onemarketingplace.es/busqueda/diseno%20web"
 
-#res = requests.get("https://onemarketingplace.es/busqueda/diseno%20web")
-#print(res)
-#re = Request(base_url)
-
-#page = urlopen(re)
-#soup = BeautifulSoup(page, "html.parser")
-#print(soup)
-#pg = soup.find("div", attrs={"id":"resultAjax"})
